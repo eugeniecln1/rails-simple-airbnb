96bba1fa0a2873fddc3e153b6c8b0910c0b0c6ae
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-20 17:21:44 +0200

    MVC done, ongoing design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Parisian appartment',
+  address: '12 rue des Lilas Paris 75006',
+  description: 'A romantic flat in the center of the capital. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Professional and clean studio New York',
+  address: '10 big avenue NY',
+  description: 'Sad and depressing. One bedroom, open plan living area, small kitchen and disgusting bathroom',
+  price_per_night: 95,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Typical Milanese house',
+  address: '12 calle derecha Milano',
+  description: 'A traditional house perfet for families. Two bedrooms, open plan living area, large kitchen and a garden',
+  price_per_night: 95,
+  number_of_guests: 2
+)
